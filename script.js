fe4e0915b1de85f876c0dfbfe5f42fda732c5b26
Piotr Kadlubski <piotrek1317@gmail.com>
2021-05-17 19:44:28 +0200

    own image fix

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function startCanvas() {
     clearCanvas()
     const image = new Image();
     image.addEventListener("load", e => {
-    ctx.drawImage(image, 0, 30, canvas.width, 256);  
+    ctx.drawImage(image, 0, 30, canvas.width, 256);
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.font = "bold 40px Impact";
     ctx.fillText(txt.upText.toUpperCase(), canvas.width / 2, 30);
     ctx.font = "bold 40px Impact";
-    ctx.fillText(txt.downText.toUpperCase(), canvas.width / 2, canvas.height - 30);
+    ctx.fillText(txt.downText.toUpperCase(), canvas.width / 2, canvas.height - 30);  
 });
 
 let txt = {
@@ -24,18 +24,20 @@ let txt = {
 const chooseImg = function() {
     let mood = document.askMood.mood;
     let fileInput = document.querySelector('#fileLoader');
-   if(fileInput.files.length !== 0) {
-    let url = URL.createObjectURL(fileInput.files[0]);
-    fileInput.value = "";
-    return url;
-   } else if (mood.value.length == 0) {
-         return 'typical.jpg';
-    }
-else
+ 
+console.log(mood.value.length)
+if (mood.value.length > 0)
 {
     return mood.value + `.jpg`
+} 
+else if (mood.value.length == 0 && fileInput.files.length == 0) {
+    return 'typical.jpg';
 }
-}
+else {
+    let url = URL.createObjectURL(fileInput.files[0]);
+    return url;
+
+}}
 image.src = chooseImg();
 }
 
@@ -49,9 +51,25 @@ function clearRadios() {
 showCanvas.height = 0;
 // czyszczenie inputów
     let allMoods = document.askMood;
-    for (let i = 0; i < allMoods.length; i++)
-    if (allMoods.mood[i].checked == true) {
+    for (let i = 0; i < allMoods.mood.length; i++) {
+        if (allMoods.mood[i].checked == true) {
         allMoods.mood[i].checked = false;
+        }
+        else {
+            continue
+        }
+    }
+}
+
+function leaveFile() {
+    let allMoods = document.askMood;
+    for (let i = 0; i < allMoods.mood.length; i++) {
+        if (allMoods.mood[i].checked == true) {
+        allMoods.mood[i].checked = false;
+        }
+        else {
+            continue
+        }
     }
 }
 
